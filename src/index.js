fd85e84d6ba9b97162329f729dd47dd6c0be6e50
Jared Mitchell <119994828+Solkind@users.noreply.github.com>
2023-05-10 22:47:19 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Here is my task list</h1>
+    <ul>
+      <li>task 1</li>
+      <li>task 2</li>
+      <li>task 3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
